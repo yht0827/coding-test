68016f12f113bc2ac59801f6f7ceadce8d7a3537
Go-EunLee <koa789456@gmail.com>
2024-01-05 16:16:51 +0900

    2024-01-05 숫자 문자열과 영단어

---
+++ Eunleelee/1월 5일/숫자_문자열과_영단어.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = ''
+    # 숫자 영어를 배열로 저장
+    number_en = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    u = ''
+    
+    for i in s:
+        u += i  # 문자열 s에서 한글자씩 문자열 u에 추가 
+        if u in number_en:  
+            answer += str(number_en.index(u))  # 문자로된 숫자를 인덱스번호로 숫자로 변환
+            u = ''
+        elif u in number:
+            answer += str(u)  # 숫자가 있으면 그대로 문자열로 변환
+            u = ''
+    
+    answer = int(answer)
+
+    return answer
