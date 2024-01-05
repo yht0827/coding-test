cc3b1ea6714a29facc898ea01c2522b45fabc534
EvelynKimm <evekimmy09@gmail.com>
2024-01-05 14:25:16 +0900

    2024-01-05 숫자 문자열과 영단어

---
+++ EvelynKimm/2024 Jan/Solution10.java
@@ -0,0 +1,12 @@
+class Solution10 {
+    public int solution(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i=0; i<arr.length; i++) {
+            if (s.contains(arr[i])){
+                s = s.replace(arr[i], Integer.toString(i));
+            }
+        }
+        return Integer.parseInt(s);
+    }
+}
