ad6e33e8969287d5d50af578dbfa8d00b18344fe
g2Min <jimin9320@gmail.com>
2024-01-06 22:49:37 +0900

    2024-01-06 크레인 인형뽑기 게임

---
+++ g2Min/01-06/solution1.java
@@ -0,0 +1,34 @@
+// 크레인 인형뽑기게임
+
+public class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.peek() == board[j][move - 1]) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(board[j][move - 1]);
+                    }
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    @Test
+    public void 정답(){
+        Assert.assertEquals(4, solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}, new int[]{1,5,3,5,1,2,1,4}));
+        Assert.assertEquals(8, solution(new int[][]{{3,3,3,3,3},{3,3,3,3,3},{3,3,3,3,3},{3,3,3,3,3},{3,3,3,3,3}}, new int[]{1,5,3,5,1,2,1,4}));
+        Assert.assertEquals(0, solution(new int[][]{{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}}, new int[]{1,5,3,5,1,2,1,4}));
+
+    }
+}
