e689ffa0e4d05a3d71c42e927c12db8d4a394fac
g2Min <jimin9320@gmail.com>
2024-01-09 22:05:19 +0900

    2024-01-09 이진변환 반복하기

---
+++ g2Min/01-09/solution2.java
@@ -0,0 +1,22 @@
+// 이진 변환 반복하기
+
+class solution2 {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while(s.length() > 1) {
+
+            int cntOne = 0;
+            for(int i=0; i<s.length(); i++) {
+
+                if(s.charAt(i) == '0') answer[1]++;
+                else cntOne++;
+            }
+
+            s = Integer.toBinaryString(cntOne);
+            answer[0]++;
+        }
+
+        return answer;
+    }
+}
