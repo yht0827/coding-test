a49f9754bc29d8d863f03d08b215c4432d093c83
EvelynKimm <evekimmy09@gmail.com>
2024-01-23 23:21:36 +0900

    2024-01-23 예산

---
+++ EvelynKimm/2024 Jan/Solution43.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution43 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+            if (budget < 0) {
+                break;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
