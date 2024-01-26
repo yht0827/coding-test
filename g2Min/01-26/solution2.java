e09071dbcb8b066b0441487ecd1ed2581a91715a
g2Min <jimin9320@gmail.com>
2024-01-26 21:40:25 +0900

    2024-01-26 광물캐기, 과일장수

---
+++ g2Min/01-26/solution2.java
@@ -0,0 +1,17 @@
+// 과일장수
+
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
+        }
+
+        return answer;
+    }
+}
