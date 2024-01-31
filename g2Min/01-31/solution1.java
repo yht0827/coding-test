e80d0374fb572f6e4b534fcff161e08112e53cfc
g2Min <jimin9320@gmail.com>
2024-01-31 22:19:54 +0900

    2024-01-31 바탕화면 정리하기, 햄버거 만들기

---
+++ g2Min/01-31/solution1.java
@@ -0,0 +1,28 @@
+//바탕화면 정리
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int minRow, minCol;
+        int maxRow, maxCol;
+
+        minRow = minCol = Integer.MAX_VALUE;
+        maxRow = maxCol = Integer.MIN_VALUE;
+
+        int row = wallpaper.length;
+        int col = wallpaper[0].length();
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (wallpaper[r].charAt(c) == '#') {
+                    minRow = Math.min(minRow, r);
+                    minCol = Math.min(minCol, c);
+                    maxRow = Math.max(maxRow, r);
+                    maxCol = Math.max(maxCol, c);
+                }
+            }
+        }
+
+        //max 값에 + 1을 하는 이유는 드래그를 하려면 끝점을 선택해야 함
+        return new int[]{minRow, minCol, maxRow + 1, maxCol + 1};
+    }
+}
