e93b6528a373e40d3d3ea41598ca4fe8e4532f2b
Go-EunLee <koa789456@gmail.com>
2024-02-02 20:43:56 +0900

    2024-02-02 음양더하기

---
+++ Eunleelee/2024-02/2월 2일/음양더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer += absolutes[i] * -1
+    return answer
