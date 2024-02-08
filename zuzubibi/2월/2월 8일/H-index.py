f628766a706fa079cbdc574dad456247084f4c25
lee_subeen <sub@likelion.org>
2024-02-08 22:48:06 +0900

    2024-02-08 하노이, HIndex

--- zuzubibi/2월/2월 8일/H-index.py
+++ zuzubibi/2월/2월 8일/H-index.py
@@ -1,6 +1,17 @@
 def solution(citations):
-    
-    return
+    answer = 0
+
+    n = len(citations)
+    citations.sort()
+
+    for i in range(n):
+        hIndex = n-i
+
+        if citations[i] >= hIndex:
+            answer= hIndex
+            break
+
+    return answer
 
 # 3
 print(solution([3,0,6,1,5]))
