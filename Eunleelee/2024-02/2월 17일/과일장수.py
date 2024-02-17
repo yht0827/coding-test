4936c94aa25bc96bafa498d406d9e69ee4d59d47
Eunleelee <121784434+Go-eunlee@users.noreply.github.com>
2024-02-17 21:04:21 +0900

    2024-02-17 과일장수

---
+++ Eunleelee/2024-02/2월 17일/과일장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    n = m - 1
+    for i in range(int(len(score)/m)):
+        answer += score[n] * m
+        n += m
+
+    return answer
