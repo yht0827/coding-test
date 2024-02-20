aab6160bfd48f1f849231688eb40e5fb0f0f95d4
lee_subeen <sub@likelion.org>
2024-02-20 21:36:21 +0900

    2024-02-20 예상 대진표, 카펫

--- zuzubibi/2월/2월 20일/예상 대진표.py
+++ zuzubibi/2월/2월 20일/예상 대진표.py
@@ -1,5 +1,11 @@
 def solution(n,a,b):
-    
-    return
+    answer= 0
+    while a!=b:
+        answer +=1
+        a,b = (a+1)//2, (b+1)//2
+        # +1 한 뒤 2로 나눈 몫을 저장하는 이유는
+        # a,b가 홀수이건 짝수이건 1을 더해서 몫으로 나누면
+        # 다음 라운드의 번호를 구할 수 있음
+    return answer
 
 print(solution(8,4,7)) # 3
--- zuzubibi/2월/2월 20일/카펫.py
+++ zuzubibi/2월/2월 20일/카펫.py
@@ -1,8 +1,23 @@
 def solution(brown, yellow):
-    answer = []
-    return answer
+    arr = []
+    for i in range(1,int(yellow**(1/2))+1):
+        if yellow % i ==0:
+            arr.append([i,yellow//i])
+    arr.sort(reverse=True)
+
+    for col, row in arr:
+        col, row = col +2, row + 2
+        if (row+col-2)*2 == brown:
+            return [row, col]
+
+def anotherSolution(brown, yellow):
+    for i in range(1, int(yellow**(1/2)+1)):
+        if yellow % i == 0:
+            if 2*(i + yellow//i) == brown-4:
+                return [yellow//i+2, i+2]
 
 
 print(solution(10,2)) # [4,3]
 print(solution(8,1)) # [3,3]
+print(solution(12,4))
 print(solution(24,24)) # [8,6]
