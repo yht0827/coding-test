4a3f0bc9753c2575dbaaadbf9596e8a16a6950ab
subeen <lsb9938@naver.com>
2024-02-20 16:54:31 +0900

    240220 문제 푸는 중

---
+++ zuzubibi/2월/2월 20일/예상 대진표.py
@@ -0,0 +1,5 @@
+def solution(n,a,b):
+    
+    return
+
+print(solution(8,4,7)) # 3
---
+++ zuzubibi/2월/2월 20일/카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    answer = []
+    return answer
+
+
+print(solution(10,2)) # [4,3]
+print(solution(8,1)) # [3,3]
+print(solution(24,24)) # [8,6]
